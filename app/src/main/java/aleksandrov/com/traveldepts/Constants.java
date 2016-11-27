6c4ee5cfc2dea9a518496099c351b964ccbe056c
Ser <seriyalexandrov@mail.ru>
2016-11-28 00:22:23 +0300

    Programmatic layout

---
+++ app/src/main/java/aleksandrov/com/traveldepts/Constants.java
@@ -0,0 +1,12 @@
+package aleksandrov.com.traveldepts;
+
+/**
+ * Created by Ser on 21/11/2016.
+ */
+public final class Constants {
+
+    public static final int DEPTS_FRAME_LAYOUT = 0;
+    public static final int DEPTS_ADD_DEPTS_BUTTON = 1;
+
+    private Constants() {}
+}
